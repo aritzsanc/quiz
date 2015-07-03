e5ab7aa5daeab4910e0e73349e2587480b7706ba
Aritz <aritzsanc@yahoo.es>
2015-07-03 09:16:34 +0200

    Vistas parciales y marco de la aplicación
    Se añade express-partials, el layaout.ejs y se modifican las páginas
    index, answer y question dejando solamente el body.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var partials = require('express-partials');
 
 var routes = require('./routes/index');
 //se elimina esta línea ya que la gestión de usuario se hará de otra forma
@@ -15,6 +16,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(partials());
 // uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
