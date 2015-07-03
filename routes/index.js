482b6bfeaa51f80b45983e82f6b80b5ca2646dbb
Aritz <aritzsanc@yahoo.es>
2015-07-03 12:46:38 +0200

    Se añade la página authors
    Se añade la página author y el link de créditos tal y como se pide en la
    práctica del módulo 6

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,9 @@ var quizController = require('../controllers/quiz_controller');
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+router.get ('/author', function(req, res) {
+  res.render('author');
+});
 
 router.get ('/quizes/question', quizController.question);
 router.get ('/quizes/answer', quizController.answer);
