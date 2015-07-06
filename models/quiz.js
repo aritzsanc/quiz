55b937c9538e803bef26dbaac43b02be297952e5
Aritz <aritzsanc@yahoo.es>
2015-07-06 14:25:00 +0200

    Se añade sequelize y sqlite

---
+++ models/quiz.js
@@ -0,0 +1,8 @@
+// Definicion del modelo de Quiz con validación
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('Quiz',
+    { 	pregunta:  DataTypes.STRING,
+		respuesta: DataTypes.STRING,
+    });
+}
